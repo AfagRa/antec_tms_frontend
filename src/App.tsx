f8ca6dc3d08349ba73f6bb9f859ad88929247aec
toghrulabdullayev <toghrulabdullayev1@gmail.com>
2026-06-17 12:16:44 +0400

    feat: add missing features

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from '@/context/AuthContext'
 import { ToastProvider } from '@/hooks/useToast'
-import { AcademicProvider } from '@/store/academicStore.tsx'
 import { router } from '@/router'
 
 export default function App() {
   return (
     <AuthProvider>
       <ToastProvider>
-        <AcademicProvider>
-          <RouterProvider router={router} />
-        </AcademicProvider>
+        <RouterProvider router={router} />
       </ToastProvider>
     </AuthProvider>
   )
